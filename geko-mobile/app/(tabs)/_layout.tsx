353a30665ee0ba70fbf95b8d71a8a397a088a916
alejoquesada <77295986+alejandroqueru@users.noreply.github.com>
2025-11-22 11:18:38 -0600

    test bottomtabs

--- geko-mobile/app/(tabs)/_layout.tsx
+++ geko-mobile/app/(tabs)/_layout.tsx
@@ -1,20 +1,20 @@
 import { Label, NativeTabs } from "expo-router/unstable-native-tabs";
 import React from "react";
 
+import { useBottomNavTab } from "@/src/features/shared/hooks/bottom-nav-tab/useBottomNavTab";
 import { useTranslation } from "@/src/features/shared/hooks/general/useTranslation";
-import { useColorScheme } from "@hooks/use-color-scheme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
   const translate = useTranslation();
+  const {hidden} = useBottomNavTab();
 
   return (
     <NativeTabs>
-      <NativeTabs.Trigger name="index">
-        <Label>{translate("helloWorld")}</Label>
+      <NativeTabs.Trigger name="index" hidden={hidden}>
+        <Label>{translate("holaMundo")}</Label>
       </NativeTabs.Trigger>
-      <NativeTabs.Trigger name="explore">
-        <Label>{translate("helloWorld")}</Label>
+      <NativeTabs.Trigger name="explore" hidden={hidden}>
+        <Label>{translate("holaMundo")}</Label>
       </NativeTabs.Trigger>
     </NativeTabs>
   );

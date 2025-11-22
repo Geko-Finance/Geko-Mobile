24184bfd1ee9da445829f809f0fc8241afb8b28a
alejoquesada <77295986+alejandroqueru@users.noreply.github.com>
2025-11-22 11:18:26 -0600

    test screen tabs

--- geko-mobile/app/(tabs)/index.tsx
+++ geko-mobile/app/(tabs)/index.tsx
@@ -1,5 +1,25 @@
-import { StyleSheet, View } from "react-native";
+import { Button } from "@/src/features/shared/components/ui/button";
+import { ScreenContainer } from "@/src/features/shared/components/ui/screen-container";
+import { UserBottomSheet } from "@/src/features/user/components/UserBottomSheet";
+import { BottomSheetMethods } from "@gorhom/bottom-sheet/lib/typescript/types";
+import { router } from "expo-router";
+import { useRef } from "react";
 
 export default function HomeScreen() {
-  return <View></View>;
+  const bottomSheetRef = useRef<BottomSheetMethods>(null);
+
+  return (
+    <ScreenContainer className="bg-black">
+      <Button
+        size="lg"
+        className="bg-red-500"
+        onPress={() => {
+          router.push("/home");
+          (bottomSheetRef as any)?.current?.expand();
+        }}
+        value="open"
+      />
+      <UserBottomSheet ref={bottomSheetRef} />
+    </ScreenContainer>
+  );
 }

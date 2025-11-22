038ddc7deda5c257b40c8358ad7d16d59a64a50c
alejoquesada <77295986+alejandroqueru@users.noreply.github.com>
2025-11-22 13:41:10 -0600

    signup screen created

---
+++ geko-mobile/app/(auth)/signup.tsx
@@ -0,0 +1,7 @@
+import { ScreenContainer } from "@/src/features/shared/components/ui/screen-container";
+
+export default function SignUpScreen() {
+  return (
+    <ScreenContainer>{/* Tu implementación de signup aquí */}</ScreenContainer>
+  );
+}
